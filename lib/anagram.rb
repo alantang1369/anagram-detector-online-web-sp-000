b083e4069c189e4afcef32b29c83b985ebef23ee
Alan Tang <giabao_tang@yahoo.com>
2020-10-08 22:35:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram 
+  attr_accessor :word 
+  def initialize(word)
+    @word = word
+  end
+  
+  def match (list_words)
+    array1 = self.word.split.sort
+    list_words.each do |element|
+      
+end
